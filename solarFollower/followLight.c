78b887a14a7d94b4d997f1f04bab0fb4eae8e661
Andre Aguillard <AguillardDre@gmail.com>
2020-02-09 23:50:41 -0600

    updated followLight program

--- solarFollower/followLight.c
+++ solarFollower/followLight.c
@@ -1,4 +1,4 @@
-/* 
+/*
      followLight.c
      gcc -o followLight followLight.c -lpigpio -lrt -lpthread
      sudo ./followLight
@@ -11,85 +11,101 @@
 
 #define BASE 100
 #define SPI_CHAN 0
+#define GPIO_UPPER 20
+#define GPIO_LOWER 21
+float map(int x);
+
+void moveLower(int A)
+{
+    gpioServo(GPIO_LOWER, A);
+}
+
+void moveUpper(int B)
+{
+    gpioServo(GPIO_UPPER, B);
+}
+
+float map(int x) //returns teh input for the servos to move to.
+{
+    float y = 500/524 * x + 1500;
+    return y;
+}
 
 int main(int argc, char *argv[])
 {
-  /*   double start;
-     if (gpioInitialise()<0)
-     {
+      double start;
+      if (gpioInitialise()<0)
+      {
           fprintf(stderr, "pigpio initalisation failed\n");
           return 1;
-     }
-     gpioSetMode(21, PI_OUTPUT); //Light up an lED
+      }
+      // Setup pins for Servo output
+      gpioSetMode(GPIO_LOWER, PI_OUTPUT); //Horizontal Servo output
+      gpioSetMode(GPIO_UPPER, PI_OUTPUT); //Vertical Servo output
 
-     // Setup input from 4 photoresitors
-     gpioSetMode(16, PI_INPUT); // topLeft  (ldrlt) 
-     gpioSetMode(20, PI_INPUT); // topRight (ldrrt)
-     gpioSetMode(12, PI_INPUT); // downLeft (ldrdl)
-     gpioSetMode(25, PI_INPUT); // downRight (ldrdr)
-     
-     // Setup pins for Servo output
-     gpioSetMode(26, PI_OUTPUT); //Horizontal Servo output
-     gpioSetMode(19, PI_OUTPUT); //Vertical Servo output
-     
-     // Set servo pulses | updates servos at 50Hz
-     gpioServo(26, 1500); //Move servo to it's midpoint
-     gpioServo(19, 1500); //Move servo to it's midpoint
-    */
-	wiringPiSetup();
- 	int i;
-	mcp3004Setup(BASE,SPI_CHAN);
-	
-     // initalize the SPI 
-     while(1) //Do the following continuously
-     {
-	for(i=0;i<8;i++)printf("Channel %d: value%4d\n",i,analogRead(BASE+i));
-	printf("\n");
-	usleep(500000);
-	
-     // Input code relating to the analog read stuff here
-     	
-	/* int lt = mcp3008_value(0, 11, 9, 8); //read top left
-	printf("%d", lt);
-	int rt = mcp3008_value(1, 11, 9, 8); // read top right
-	int ld = mcp3008_value(2, 11, 9, 8); //read down left
-	int rd = mcp3008_value(3, 11, 9, 8); //read down right
-	printf("%d", rt);
-	printf("%d", ld);
-	printf("%d", rd);
-    	*/
-	
-/*  
+      // Set servo pulses | updates servos at 50Hz
+      gpioServo(GPIO_LOWER, 1500); //Move servo to it's midpoint LOWER
+
+      gpioServo(GPIO_UPPER, 1500); //Move servo to it's midpoint UPPER
+
+      wiringPiSetup();
+      int i;
+      mcp3004Setup(BASE,SPI_CHAN);
+
+      // initalize the SPI
+      while(1) //Do the following continuously
+      {
+          for(i=0;i<8;i++)printf("Channel %d: value%4d\n",i,analogRead(BASE+i));
+          printf("\n");
+
+          int tr = analogRead(BASE+ 0); // Top right
+          int tl = analogRead(BASE+ 1); //
+          int br = analogRead(BASE+ 5); //
+          int bl = analogRead(BASE+ 7); //
+
+          int avgRight = (tr+tl)/2;
+          int avgLeft = (tl+bl)/2;
+          int avgBottom = (br+bl)/2;
+          int avgTop = (tr+tl)/2;
+
+          int A = avgLeft - avgRight;
+          int B = avgTop - avgBottom;
+
+          if ((A > 0 && B > 0) || (A < 0 && B > 0))
+          {
+              moveLower(map(A));
+              moveUpper(map(B));
+          }
+          if ((A > 0 && B > 0) || (A < 0 && B > 0))
+          {
+              moveLower(map(A));
+              moveUpper(map(B));
+          }
+
+
+
+          usleep(500000);
+
+
+/*
      int dtime = 10;
      int tol = 50;
 
-     int avt = (lt + rt) / 2; // average value top
-     int avd = (ld + rd) / 2; // average value down
-     int avl = (lt + ld) / 2; // average value left
-     int avr = (rt + rd) / 2; // average value right
 
      dvert = avt-avd; // check the difference of up and down
      dhoriz = avl - avr; // check the difference of left and right
-     
+
      if (-1*tol > dvert || dvert > tol) // check if the difference is in the tolerance else change the vertical angle
      {
      if (avt > avd)
      {
-           
 
 
 
-     }
 
-     start = time_time();
-     while((time_time() - start) < 60.0)
-     {
-          gpioWrite(21,1);
-	  time_sleep(0.5);
-	  gpioWrite(21,0);
-	  time_sleep(0.5);
      }
      gpioTerminate();*/
-}     
+}
+gpioTerminate();
 return 0;
 }
